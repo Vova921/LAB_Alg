1331ed926e8a68f0034c92c0369ad02b2553c600
Vova921 <89463208+Vova921@users.noreply.github.com>
2021-09-30 12:37:58 +0300

    Add files via upload

---
+++ 1_1_3_Omelianchuk.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    setlocale(LC_ALL, "ukr");
+    int a, a2, a3, a6, a7;
+    cout << "Введіть ціле число а:" << endl;
+    cin >> a;
+
+    a2 = a * a;
+    a3 = a2 * a;
+    a6 = a3 * a3;
+    a7 = a6 * a;
+
+    cout << "а7 = " << a7;
+
+
+}
+
