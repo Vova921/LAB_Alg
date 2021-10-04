520e91a2e61726736fdd3246aba2b72d0ab8f5d4
Vova921 <89463208+Vova921@users.noreply.github.com>
2021-10-04 19:18:06 +0300

    Add files via upload

---
+++ Lab_4 Alg/4_3_Omelianchuk.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ukr");
+	const float p0 = 1.29;
+	const float z = 1.25 * pow(10, -4);
+	const float e = 2.71828;
+	float p;
+
+	cout << "Висота" << "      " << "Густина" << endl;
+	for (int h = 0; h <= 1000; h += 100) {
+		p = p0 * pow(e, -1 * h * z);
+		cout << "   " << h << " м     " << p<<" кг/м3"<< endl;
+    }
+}
